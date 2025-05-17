ad3099e67ef388b102e5edaebee1610211a800a4
LichtBach <116815525+HadesTheHidden@users.noreply.github.com>
2025-05-17 07:06:24 +0530

    Services Test Suite Added

--- src/test/java/com/cg/training/service/BookTest.java
+++ src/test/java/com/cg/training/service/BookTest.java
@@ -1,6 +1,45 @@
 package com.cg.training.service;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.function.ThrowingRunnable;
 
 public class BookTest {
-    
+	@Test
+	public void testBookCreation() {
+		Book book = new Book("Effective Java", "Joshua Bloch");
+		assertEquals("Effective Java", book.getTitle());
+		assertEquals("Joshua Bloch", book.getAuthor());
+		assertTrue(book.isAvailable());
+	}
+
+	@Test
+	public void testEmptyTitle() {
+		assertThrows(IllegalArgumentException.class, new ThrowingRunnable() {
+			@Override
+			public void run() throws Throwable {
+				new Book("", "Author");
+			}
+		});
+	}
+
+	@Test
+	public void testEmptyAuthor() {
+		assertThrows(IllegalArgumentException.class, new ThrowingRunnable() {
+			@Override
+			public void run() throws Throwable {
+				new Book("Title", "");
+			}
+		});
+	}
+
+	@Test
+	public void testAvailability() {
+		Book book = new Book("Test", "Author");
+		assertTrue(book.isAvailable());
+//		book.setAvailable(false);
+	}
 }
--- src/test/java/com/cg/training/service/LoanTest.java
+++ src/test/java/com/cg/training/service/LoanTest.java
@@ -1,5 +1,43 @@
 package com.cg.training.service;
 
-public class LoanTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import com.cg.training.model.Member;
+import com.cg.training.model.UserDTO;
+
+public class LoanTest {
+    private Member member;
+    private Book book;
+    
+    @Before
+    public void setUp() {
+        member = new Member(new UserDTO("Test Member"));
+        book = new Book("Test Book", "Test Author");
+    }
+    
+    @Test
+    public void testLoanCreation() {
+        Loan loan = new Loan(member, book);
+        assertEquals("Borrowed", loan.getStatus());
+    }
+    
+    @Test
+    public void testCompleteLoan() {
+        Loan loan = new Loan(member, book);
+        loan.completeLoan();
+        assertEquals("Returned", loan.getStatus());
+        assertTrue(book.isAvailable());
+    }
+    
+    @Test
+    public void testLoanDetails() {
+        Loan loan = new Loan(member, book);
+        String details = loan.loanDetails();
+        assertTrue(details.contains("Test Member"));
+        assertTrue(details.contains("Test Book"));
+    }
 }
